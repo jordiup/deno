53289155ab359e10692c0af095dfa772323fa9b3
Jordi <jordi.hermoso.up@gmail.com>
2020-07-14 11:05:14 +0800

    Planet project init.

---
+++ playground/deno4.ts
@@ -0,0 +1,6 @@
+import { denode } from "./deno3.ts"; // note: Deno explicitly needs file extensions
+import "https://deno.land/std/examples/welcome.ts";
+
+console.log(denode("jskljlsjdfl"));
+console.log(denode("deno"));
+console.log(denode("node"));
