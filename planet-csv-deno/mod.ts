dc73e09d44c859553172a0aa9f11b7ac2ece794f
Jordi <jordi.hermoso.up@gmail.com>
2020-08-16 20:52:37 +0800

    bufReader for csv reads

--- planet-csv-deno/mod.ts
+++ planet-csv-deno/mod.ts
@@ -1,11 +1,20 @@
 import { join } from "https://deno.land/std/path/mod.ts";
+import { BufReader } from "https://deno.land/std/io/bufio.ts";
+import { parse } from "https://deno.land/std/encoding/csv.ts";
 
-async function readFile() {
-  const path = join("text_files", "hello.txt"); // join allows you to join directories
+async function loadPlanetsData() {
+  const path = join(".", "kepler_exoplanets_nasa.csv"); // join allows you to join directories
 
-  const data = await Deno.readTextFile(path);
+  const file = await Deno.open(path);
+  const bufReader = new BufReader(file);
 
-  console.log(data);
+  const result = await parse(bufReader, {
+    header: true,
+    comment: "#",
+  });
+  Deno.close(file.rid); // close the file (using its resource id)
+
+  console.log(result);
 }
 
-await readFile();
+await loadPlanetsData();
